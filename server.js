1fc1c1d526d9562818f63a492827c35bb73dd130
Akash Chanara <akashchanara@gmail.com>
2022-10-27 21:30:26 -0700

    get requset

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
-const api = require("./public/assets/js/index");
+const path = require("path");
+const fs = require("fs");
+const api = require("./public/assets/js/index.js");
 const PORT = process.env.port || 3002;
-
+const db = require("./db/db.json");
 const app = express();
 app.use("/api", api);
+
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+app.get("/api/notes", (req, res) => {
+  return res.json(db);
+});
+app.post("/api/notes", (req, res) => {
+  let note = req.body;
+  db.push(note);
+  fs.writeFileSync("./db/db.json", JSON.stringify(db));
+  res.json(db);
+});
+app.get("/api/notes");
 app.listen(PORT, () =>
   console.log(`App listenig at http://localhost:${PORT} 🚀`)
 );
