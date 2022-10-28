3f4a6f6e34af671f64dc439ca2f7fef05dc0489f
Akash Chanara <akashchanara@gmail.com>
2022-10-27 23:39:16 -0700

    deploy page solve error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const PORT = process.env.port || 3002;
+const PORT = process.env.PORT || 3002;
 const db = require("./db/db.json");
 const app = express();
 // app.use("/api", api);
