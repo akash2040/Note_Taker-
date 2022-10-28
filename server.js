f9d944fee170878cae4cfac0951c8a607064f6c6
Akash Chanara <akashchanara@gmail.com>
2022-10-28 12:26:33 -0700

    Post req error

--- server.js
+++ server.js
@@ -17,14 +17,53 @@ app.get("/notes", (req, res) =>
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
 );
+// app.get("/api/notes", (req, res) => {
+//   return res.json(db);
+// });
+// app.post("/api/notes", (req, res) => {
+//   let note = req.body;
+//   db.push(note);
+//   fs.writeFileSync("./db/db.json", JSON.stringify(db));
+//   res.json(db);
+// });
 app.get("/api/notes", (req, res) => {
-  return res.json(db);
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+    if (error) {
+      return console.log(error);
+    }
+    res.json(JSON.parse(notes));
+  });
 });
+var noteNewarr;
 app.post("/api/notes", (req, res) => {
-  let note = req.body;
-  db.push(note);
-  fs.writeFileSync("./db/db.json", JSON.stringify(db));
-  res.json(db);
+  const note = req.body;
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+    if (error) {
+      return console.log(error);
+    }
+    noteNewarr = JSON.parse(notes);
+    var noteId = noteNewarr.length;
+
+    let newNote = {
+      title: note.title,
+      text: note.text,
+      id: noteId,
+    };
+
+    var newArr = noteNewarr.concat(newNote);
+
+    fs.writeFile(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify(newArr),
+      (error, data) => {
+        if (error) {
+          return error;
+        }
+        console.log(newArr);
+        res.json(newArr);
+      }
+    );
+  });
 });
 // app.delete("/api/notes:id", (req, res) => {
 //   let id = req.params.id;
