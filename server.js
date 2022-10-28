0f0bf6e5341ecfe65099ab15e16d619465ac38e5
Akash Chanara <akashchanara@gmail.com>
2022-10-28 02:22:59 -0700

    complete project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.port || 3002;
 const db = require("./db/db.json");
 const app = express();
 // app.use("/api", api);
@@ -14,7 +14,7 @@ app.use(express.static("./public"));
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
-app.get("*", (req, res) =>
+app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 app.get("/api/notes", (req, res) => {
